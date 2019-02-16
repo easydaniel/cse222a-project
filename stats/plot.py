bc8da5c912266fbe23ec5312f28d1d823c2cc38a
Ubuntu <ubuntu@ip-172-31-25-25.us-west-2.compute.internal>
2019-02-16 23:12:05 +0000

    10-501 dataset

--- stats/plot.py
+++ stats/plot.py
@@ -8,11 +8,11 @@ def plot():
     dataset = [list(map(float, v.split()))
                for v in open(REQLAT_PATH, 'r').readlines()]
     frate, lat = [], []
-    xlabels = list(range(10, 201, 10))
+    xlabels = list(range(10, 501, 10))
     idx = 0
     for _ in range(4):
         tf, tl = [], []
-        for _ in range(10, 201, 10):
+        for _ in range(10, 501, 10):
             tf.append(dataset[idx][0])
             tl.append(dataset[idx][1])
             idx += 1
