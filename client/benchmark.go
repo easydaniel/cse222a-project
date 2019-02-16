77a1f0217c2b10138ecc11776f71ba87cde2c673
easydaniel <tanchientw@outlook.com>
2019-02-15 18:25:03 -0800

    update plot.py

--- client/benchmark.go
+++ client/benchmark.go
@@ -3,7 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"sync"
 	"time"
 )
 
@@ -12,6 +15,8 @@ type Status struct {
 	latency time.Duration
 	code    int
 }
+
+var wg sync.WaitGroup
 
 func qs(params map[string]string) string {
 	if len(params) == 0 {
@@ -24,17 +29,37 @@ func qs(params map[string]string) string {
 	return s[:len(s)-1]
 }
 
-func request(url string, reqCnt int, params map[string]string, message chan Status) {
+func genParam(conType int) map[string]string {
+	param := make(map[string]string)
+	p := 0
+	if conType == 3 {
+		r := rand.Intn(2)
+		if r == 1 {
+			p = 500
+		} else {
+			p = 100
+		}
+	} else if conType == 2 {
+		p = 500
+	} else if conType == 1 {
+		p = 100
+	}
+	param["period"] = strconv.Itoa(p)
+	return param
+}
 
-	qstr := qs(params)
-	endpoint := fmt.Sprintf("%s?%s", url, qstr)
+func request(url string, reqCnt int, conType int, message chan Status) {
+	defer wg.Done()
+
 	for idx := 0; idx < reqCnt; idx++ {
+		qstr := qs(genParam(conType))
+		endpoint := fmt.Sprintf("%s?%s", url, qstr)
 		start := time.Now()
 		resp, err := http.Get(endpoint)
 		latency := time.Since(start)
 		code := -1
 		if err != nil {
-			// fmt.Println(err)
+			fmt.Println(err)
 		} else {
 			code = resp.StatusCode
 			resp.Body.Close()
@@ -52,20 +77,30 @@ func min(a, b int) int {
 
 func main() {
 	var numReq int
-	flag.IntVar(&numReq, "n", 10000, "# of requests")
+	flag.IntVar(&numReq, "n", 1000, "# of requests")
 	var numCon int
 	flag.IntVar(&numCon, "c", 10, "# of connections")
+	var conType int
+	flag.IntVar(&conType, "t", 0, "type of connection: 1: short, 2: long, 3: mix")
 	flag.Parse()
+	if len(flag.Args()) != 1 {
+		fmt.Printf("Usage: benchmark [options] <url>\n")
+		return
+	}
 	message := make(chan Status)
 	status := make(map[int][]float32)
 	numEach := numReq / numCon
 	for total := numReq; total > 0; total -= numEach {
-		go request("http://allen.nctu.me:8388/", min(total, numEach), make(map[string]string), message)
+		wg.Add(1)
+		go request(flag.Arg(0), min(total, numEach), conType, message)
 	}
-	for idx := 0; idx < numReq; idx++ {
-		result := <-message
-		status[result.code] = append(status[result.code], (float32(result.latency) / 1000000))
-	}
+
+	go func() {
+		for result := range message {
+			status[result.code] = append(status[result.code], (float32(result.latency) / 1000000))
+		}
+	}()
+	wg.Wait()
 	close(message)
 	// fmt.Fprintf(os.Stderr, "Total: %d, Num: %d, Con: %d\n", len(status[200]), numReq, numCon)
 	failRate := float32(len(status[-1])) / float32(numReq)
--- stats/plot.py
+++ stats/plot.py
@@ -1,9 +1,28 @@
 from matplotlib import pyplot as plt
 import seaborn as sns
 
+REQLAT_PATH = './reqlat.ds'
+
 
 def plot():
-    pass
+    dataset = [list(map(float, v.split()))
+               for v in open(REQLAT_PATH, 'r').readlines()]
+    frate, lat = [], []
+    xlabels = list(range(10, 201, 10))
+    idx = 0
+    for _ in range(4):
+        tf, tl = [], []
+        for _ in range(10, 201, 10):
+            tf.append(dataset[idx][0])
+            tl.append(dataset[idx][1])
+            idx += 1
+        frate.append(tf)
+        lat.append(tl)
+
+    for i in range(4):
+        sns.lineplot(x=xlabels, y=lat[i], label=f'{i}')
+
+    plt.show()
 
 
 if __name__ == "__main__":
