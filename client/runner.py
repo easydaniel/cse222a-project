09b2eaac9b166bfc0041983b8c1a1b0db09a026e
Ubuntu <ubuntu@ip-172-31-16-149.us-west-2.compute.internal>
2019-03-20 03:19:08 +0000

    add eps

--- client/runner.py
+++ client/runner.py
@@ -6,9 +6,12 @@ LL_EP = 'http://52.41.245.156:3019'
 PA_EP = 'http://52.42.191.169:3019'
 SERV_EP = 'http://dev-env-easy.jvrb83sqrz.us-west-2.elasticbeanstalk.com/main/time'
 
+EP1 = '/main/time'
+EP2 = '/main/notServerOtherPeriod'
+
 for T in range(4):
     for C in range(20, 501, 20):
         call(
-            './benchmark -n 30 -c %d -t %d -s %d %s' % (C, 3, 2, PA_EP + '/main/time'), shell=True)
+            './benchmark -n 30 -c %d -t %d -s %d %s' % (C, 3, 2, LL_EP + EP2), shell=True)
         sleep(5)
     break
